3ac53b5b6e4933371bec909df5e1b1bee9fec8df
mcmiloy <mcmiloy@gmail.com>
2018-05-19 08:39:50 +0200

    first commit

---
+++ revealing_references.rb
@@ -0,0 +1,24 @@
+class RevealingReferences
+    attr_reader :wheels
+    def initialize(data)
+        @wheels = wheelify(data)
+    end
+
+    def diameters
+        wheels.collect {|wheel| diameter(wheel)}
+    end
+
+    def diameter(wheel)
+        wheel.rim + (wheel.tire * 2)
+    end
+
+    Wheel = Struct.new(:rim, :tire)
+    def wheelify(data) # isolates messy structural information
+        data.collect do |cell|
+            Wheel.new(cell[0], cell[1]) 
+        end
+    end
+end
+
+
+
---
+++ test.rb
@@ -0,0 +1,6 @@
+myhash = {emilio: 19, keno: 51, laura: 54};
+
+myhash.each do |key, value|
+    puts "key: #{key}, value: #{value}"
+end
+
